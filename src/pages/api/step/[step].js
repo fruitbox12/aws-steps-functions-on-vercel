ae508dc1f96621ddc468379126cd283b0bf0e0fe
fruitbox12 <dylanwong007@gmail.com>
2024-02-22 16:19:40 -0800

    Update [step].js

--- src/pages/api/step/[step].js
+++ src/pages/api/step/[step].js
@@ -45,7 +45,7 @@ export default async (req, res) => {
             res.writeHead(307, { Location: `/api/step/${nextStepIndex}?stepEnd=${stepEnd}` });
             res.end();
         } else {
-            res.status(200).json({ results: existingResults });
+res.status(200).json({ root: existingResults });
         }
     } catch (error) {
         console.error(`Error executing step ${stepIndex}:`, error);
