847b4391cf8c6bd94936d043cad69b9df818b2e6
Hector Garcia <hmgarcia06@gmail.com>
2024-05-06 08:37:23 -0400

    refactored: redefined imports from relative to absolutes, removed whitespace and improved indentations

--- src/pages/api/step/[step].js
+++ src/pages/api/step/[step].js
@@ -1,18 +1,20 @@
 
-import { executeHttpNode } from '../../../utils/httpRequestExecutor';
-import { getWorkflowState,setWorkflowNodeState,getWorkflowNodeState, setWorkflowState } from '../../../utils/kvStorage';
 import NextCors from 'nextjs-cors';
-import { registerCron } from '../../../utils/cronUtils'; // Assuming this utility is correctly implemented
-import { webhookHttpNode } from '../../../utils/webhookUtil'; // Assuming this utility is correctly implemented
-import { replaceTemplateVariables, replaceTemplateBody } from '../../../utils/regex'; // Assuming this utility is correctly implemented
-import { executeDiscordNode } from '../../../utils/Discord'; // Assuming this utility is correctly implemented
-import { executeNodeJs } from '../../../utils/nodejs'; // Assuming this utility is correctly implemented
+
+import { executeHttpNode } from 'utils/httpRequestExecutor';
+import { getWorkflowState,setWorkflowNodeState,getWorkflowNodeState, setWorkflowState } from 'utils/kvStorage';
+import { registerCron } from 'utils/cronUtils'; // Assuming this utility is correctly implemented
+import { webhookHttpNode } from 'utils/webhookUtil'; // Assuming this utility is correctly implemented
+import { replaceTemplateVariables, replaceTemplateBody } from 'utils/regex'; // Assuming this utility is correctly implemented
+import { executeDiscordNode } from 'utils/Discord'; // Assuming this utility is correctly implemented
+import { executeNodeJs } from 'utils/nodejs'; // Assuming this utility is correctly implemented
+import { generateShortId } from 'utils/string'; // This function was defined here but moved to a different utils file.
 
 const { MongoClient } = require('mongodb');
 export const config = {
   maxDuration: 300,
 };
- 
+
 export default async (req, res) => {
     await NextCors(req, res, {
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
@@ -39,148 +41,126 @@ export default async (req, res) => {
             existingResults = [];
         }
 
-try {        let previousNodeOutput = {};
+        try {     
+            let previousNodeOutput = {};
 
-    if (nodes[stepIndex].data.type === 'trigger') {
-        // Assuming registerCron function returns some result or confirmation
-                                                                // TODO: Fix this shit, +1 is hard coded but should be getCronNodes => nodes.pop(0) so index 1 is now 0 but the rest of the indexes are present (delete index 0)
-        const cronResult = await registerCron(nodes[stepIndex], [nodes[stepIndex+1]], shortId, tenantId);
-        existingResults.push({ cronResult });
-    }
-    else if (nodes[stepIndex].data.type === 'webhook') {
-       await setWorkflowNodeState(trigger_output, nodes[stepIndex].id, [{ data: webhook_body }])
-        
-    } 
-        else if (nodes[stepIndex].data.name === 'discord') {
-  const previousNodeId = nodes[stepIndex - 1].id;
-  previousNodeOutput = await getWorkflowNodeState(trigger_output);
-         const nodeInput = replaceTemplateVariables(nodes[stepIndex].data?.inputParameters?.webhookUrl, previousNodeOutput);
-         const nodeBody = replaceTemplateBody(nodes[stepIndex].data?.inputParameters?.content, previousNodeOutput);
+            if (nodes[stepIndex].data.type === 'trigger') {
+                // Assuming registerCron function returns some result or confirmation
+                // TODO: Fix this sh1t, +1 is hard coded but should be getCronNodes => nodes.pop(0) so index 1 is now 0 but the rest of the indexes are present (delete index 0)
+                const cronResult = await registerCron(nodes[stepIndex], [nodes[stepIndex+1]], shortId, tenantId);
+                existingResults.push({ cronResult });
+            }
+            else if (nodes[stepIndex].data.type === 'webhook') {
+                await setWorkflowNodeState(trigger_output, nodes[stepIndex].id, [{ data: webhook_body }])    
+            } 
+            else if (nodes[stepIndex].data.name === 'discord') {
+                const previousNodeId = nodes[stepIndex - 1].id;
+                previousNodeOutput = await getWorkflowNodeState(trigger_output);
+                const nodeInput = replaceTemplateVariables(nodes[stepIndex].data?.inputParameters?.webhookUrl, previousNodeOutput);
+                const nodeBody = replaceTemplateBody(nodes[stepIndex].data?.inputParameters?.content, previousNodeOutput);
 
-// Update the currentNode with the new inputParameters.url value
-nodes[stepIndex].data.inputParameters.webhookUrl = nodeInput;
-nodes[stepIndex].data.inputParameters.content = nodeBody;
+                // Update the currentNode with the new inputParameters.url value
+                nodes[stepIndex].data.inputParameters.webhookUrl = nodeInput;
+                nodes[stepIndex].data.inputParameters.content = nodeBody;
 
-// Execute the HTTP Node with the updated currentNode
-const data = await executeDiscordNode(nodes[stepIndex]);
-existingResults.push({ data: data });
-await setWorkflowNodeState(trigger_output, nodes[stepIndex].id, [{ data: data }]);
-        
-    } 
-             else if (nodes[stepIndex].data.name === 'nodeJS') {
-  const previousNodeId = nodes[stepIndex - 1].id;
-  previousNodeOutput = await getWorkflowNodeState(trigger_output);
-         const nodeInput = replaceTemplateVariables(nodes[stepIndex].data?.inputParameters?.code, previousNodeOutput);
-         const nodeBody = replaceTemplateBody(nodes[stepIndex].data?.inputParameters?.external, previousNodeOutput);
+                // Execute the HTTP Node with the updated currentNode
+                const data = await executeDiscordNode(nodes[stepIndex]);
+                existingResults.push({ data: data });
+                await setWorkflowNodeState(trigger_output, nodes[stepIndex].id, [{ data: data }]);
+            } 
+            else if (nodes[stepIndex].data.name === 'nodeJS') {
+                const previousNodeId = nodes[stepIndex - 1].id;
+                previousNodeOutput = await getWorkflowNodeState(trigger_output);
+                const nodeInput = replaceTemplateVariables(nodes[stepIndex].data?.inputParameters?.code, previousNodeOutput);
+                const nodeBody = replaceTemplateBody(nodes[stepIndex].data?.inputParameters?.external, previousNodeOutput);
 
-// Update the currentNode with the new inputParameters.url value
-nodes[stepIndex].data.inputParameters.code = nodeInput;
-nodes[stepIndex].data.inputParameters.external = nodeBody;
+                // Update the currentNode with the new inputParameters.url value
+                nodes[stepIndex].data.inputParameters.code = nodeInput;
+                nodes[stepIndex].data.inputParameters.external = nodeBody;
 
-// Execute the HTTP Node with the updated currentNode
-const data = await executeNodeJs(nodes[stepIndex]);
-existingResults.push({ data: data });
-await setWorkflowNodeState(trigger_output, nodes[stepIndex].id, [{ data: data }]);
-        
-    } 
-        else if (stepIndex > 0) {
+                // Execute the HTTP Node with the updated currentNode
+                const data = await executeNodeJs(nodes[stepIndex]);
+                existingResults.push({ data: data });
+                await setWorkflowNodeState(trigger_output, nodes[stepIndex].id, [{ data: data }]);
+            } 
+            else if (stepIndex > 0) {
+               const previousNodeId = nodes[stepIndex - 1].id;
+               const previousNodeOutput = await getWorkflowNodeState(trigger_output);
+               const nodeInput = replaceTemplateVariables(nodes[stepIndex].data?.inputParameters?.url, previousNodeOutput);
+               const nodeBody = replaceTemplateBody(nodes[stepIndex].data?.inputParameters?.body, previousNodeOutput);
 
-             const previousNodeId = nodes[stepIndex - 1].id;
-            previousNodeOutput = await getWorkflowNodeState(trigger_output);
-         const nodeInput = replaceTemplateVariables(nodes[stepIndex].data?.inputParameters?.url, previousNodeOutput);
-         const nodeBody = replaceTemplateBody(nodes[stepIndex].data?.inputParameters?.body, previousNodeOutput);
+                // Update the currentNode with the new inputParameters.url value
+               nodes[stepIndex].data.inputParameters.url = nodeInput;
+               nodes[stepIndex].data.inputParameters.body = nodeBody;
 
-// Update the currentNode with the new inputParameters.url value
-nodes[stepIndex].data.inputParameters.url = nodeInput;
-nodes[stepIndex].data.inputParameters.body = nodeBody;
+                // Execute the HTTP Node with the updated currentNode
+               const data = await executeHttpNode(nodes[stepIndex]);
+               existingResults.push({ data: data });
+               await setWorkflowNodeState(trigger_output, nodes[stepIndex].id, [{ data: data }]);
+            } else {  
+                // const data = await webhookHttpNode(nodes[stepIndex], nodes, existingResults[existingResults.length - 1]);
+                const data = await executeHttpNode(nodes[stepIndex]);
+                await setWorkflowNodeState(trigger_output, nodes[stepIndex].id, [{ data: data }]);
+                // Insert the document into the collection
 
-// Execute the HTTP Node with the updated currentNode
-const data = await executeHttpNode(nodes[stepIndex]);
-existingResults.push({ data: data });
-await setWorkflowNodeState(trigger_output, nodes[stepIndex].id, [{ data: data }]);
-
-        } else {  
-
-    
-//              const data = await webhookHttpNode(nodes[stepIndex], nodes, existingResults[existingResults.length - 1]);
-
-              const data = await executeHttpNode(nodes[stepIndex]);
-           
-            
-await setWorkflowNodeState(trigger_output, nodes[stepIndex].id, [{ data: data }]);
-
-
-// Insert the document into the collection
-
-// Then, push the constructed object to the array
-existingResults.push({ data: data });
-    }
-} catch (error) {
-    // Log the error to the console
-    console.error(`Error executing: ${error}`);
-    // Push the error message or error object to existingResults for later processing
-    existingResults.push({ error: error.message || 'Unknown error' });
-           
-
-}
- await setWorkflowState(shortId, existingResults);
+                // Then, push the constructed object to the array
+                existingResults.push({ data: data });
+            }
+        } catch (error) {
+            // Log the error to the console
+            console.error(`Error executing: ${error}`);
+            // Push the error message or error object to existingResults for later processing
+            existingResults.push({ error: error.message || 'Unknown error' });
+        }
+        await setWorkflowState(shortId, existingResults);
 
         if (stepIndex < stepEnd) {
             const nextStepIndex = stepIndex + 1;
             res.writeHead(307, { Location: `/api/step/${nextStepIndex}?stepEnd=${stepEnd}` });
             res.end();
         } else {
+        // Connection URL and Database Name
+        // Assuming `db` is your database instance and `req`, `res` are Express request and response objects
+        // Function to insert a new execution and update the workflow with execution data
+        try {
+            const url = 'mongodb+srv://dylan:43VFMVJVJUFAII9g@cluster0.8phbhhb.mongodb.net/?retryWrites=true&w=majority';
+            const dbName = 'test';
+            const client = new MongoClient(url);
+            await client.connect();
 
-// Connection URL and Database Name
+            const db = client.db(dbName);
 
-// Assuming `db` is your database instance and `req`, `res` are Express request and response objects
+            // Generate a shortId for the execution
 
-// Function to insert a new execution and update the workflow with execution data
-    try {
-        const url = 'mongodb+srv://dylan:43VFMVJVJUFAII9g@cluster0.8phbhhb.mongodb.net/?retryWrites=true&w=majority';
-const dbName = 'test';
-const client = new MongoClient(url);
-await client.connect();
+            // New execution object
+            let execution = {
+                _id: null, // This will be set by your database
+                executionData:  JSON.stringify([{nodeId:  nodes[stepIndex].id, nodeLabel: nodes[stepIndex].data.label, data: existingResults, status: "FINISHED"}]) , // Populate as necessary
+                state: "SUCCESS", // Or "SUCCESS" or "FAILED" based on your logic
+                workflowShortId: shortId,
+                shortId: trigger_output || "E" + shortId ,
+                // Passed in the request body
+                createdDate: new Date(),
+                stoppedDate: new Date() // Set this when the execution stops
+            };
 
-const db = client.db(dbName);
+            // Insert the new execution into the database
+            const executionRepository = db.collection(`execution_${tenantId}`);
+            await executionRepository.insertOne(execution);
 
-        // Generate a shortId for the execution
-
-        // New execution object
-        let execution = {
-            _id: null, // This will be set by your database
-            executionData:  JSON.stringify([{nodeId:  nodes[stepIndex].id, nodeLabel: nodes[stepIndex].data.label, data: existingResults, status: "FINISHED"}]) , // Populate as necessary
-            state: "SUCCESS", // Or "SUCCESS" or "FAILED" based on your logic
-            workflowShortId: shortId,
-            shortId: trigger_output || "E" + shortId ,
-            // Passed in the request body
-            createdDate: new Date(),
-            stoppedDate: new Date() // Set this when the execution stops
-        };
-
-        // Insert the new execution into the database
-        const executionRepository = db.collection(`execution_${tenantId}`);
-        await executionRepository.insertOne(execution);
-
-        // Retrieve updated execution data for the workflow
-        const executionData = await executionRepository.find({ workflowShortId: shortId }).toArray();
-        // Assuming 'workflow' is already defined or retrieved from the database
- 
-        // Respond with the updated workflow object
-    } catch (error) {
-        console.error('Error handling execution:', error);
-        // Handle error, e.g., return an error response
-    }
-
-
-res.status(200).json({ data: existingResults });
+            // Retrieve updated execution data for the workflow
+            const executionData = await executionRepository.find({ workflowShortId: shortId }).toArray();
+            // Assuming 'workflow' is already defined or retrieved from the database
+    
+            // Respond with the updated workflow object
+        } catch (error) {
+            console.error('Error handling execution:', error);
+            // Handle error, e.g., return an error response
+        }
+            res.status(200).json({ data: existingResults });
         }
     } catch (error) {
         console.error(`Error executing step ${stepIndex}:`, error);
         res.status(204).json({ error: `Error executing step ${stepIndex}` });
     }
 };
-
-function generateShortId(prefix) {
-    return `${prefix}-${Math.random().toString(36).substr(2, 9)}`;
-}
